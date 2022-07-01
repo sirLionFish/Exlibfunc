ef63297d990e220ea66fdaa8ac537033c77279b9
sirlionfish <justin.ke@deemoney.com>
2022-07-02 02:08:30 +0700

    first week

---
+++ lib/exlibfunc/libraries/july/first_week.ex
@@ -0,0 +1,25 @@
+defmodule Exlibfunc.Libraries.July.FirstWeek do
+
+# ------------level 1----------------
+  @doc """
+    1.) Make a simple function called greet that returns the most-famous "hello world!".
+  """
+
+  @doc """
+    2.) Simple, remove the spaces from the string, then return the resultant string.
+  """
+
+  @doc """
+    3.) Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+  """
+
+  @doc """
+    4.) Make a function that takes in an integer, separate into individual digits, square them, and then concatnate them.
+  """
+
+
+# ------------level 2----------------
+
+# ------------level 3----------------
+
+end
